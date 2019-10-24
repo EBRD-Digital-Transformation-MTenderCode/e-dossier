2c58ac350c7bf3aaa8bd738d78452f3a0181945a
Evgen Kopach <zkopach@gmail.com>
2019-10-24 19:09:12 +0300

    ES-2009 Created request DTO for 'checkResponses' command

--- src/main/kotlin/com/procurement/procurer/infrastructure/model/dto/cn/CheckCriteriaRequest.kt
+++ src/main/kotlin/com/procurement/procurer/infrastructure/model/dto/cn/CheckCriteriaRequest.kt
@@ -5,12 +5,12 @@ import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.procurement.procurer.application.model.data.CoefficientValue
+import com.procurement.procurer.application.model.data.Requirement
 import com.procurement.procurer.infrastructure.bind.coefficient.CoefficientValueDeserializer
 import com.procurement.procurer.infrastructure.bind.coefficient.CoefficientValueSerializer
 import com.procurement.procurer.infrastructure.bind.criteria.RequirementDeserializer
 import com.procurement.procurer.infrastructure.bind.criteria.RequirementSerializer
-import com.procurement.procurer.application.model.data.CoefficientValue
-import com.procurement.procurer.application.model.data.Requirement
 import com.procurement.procurer.infrastructure.model.dto.ocds.AwardCriteria
 import com.procurement.procurer.infrastructure.model.dto.ocds.AwardCriteriaDetails
 import com.procurement.procurer.infrastructure.model.dto.ocds.ConversionsRelatesTo
