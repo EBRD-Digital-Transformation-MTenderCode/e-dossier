93fb27996af0f30918a0e236e835eee7012efaf2
Evgen Kopach <zkopach@gmail.com>
2019-10-24 19:23:39 +0300

    ES-2009 Created data object for requestDTO ('checkResponses' command)

---
+++ src/main/kotlin/com/procurement/procurer/infrastructure/model/dto/cn/CheckResponsesData.kt
@@ -0,0 +1,39 @@
+package com.procurement.procurer.infrastructure.model.dto.cn
+
+import com.procurement.procurer.application.model.data.RequirementRsValue
+import java.time.LocalDateTime
+
+data class CheckResponsesData(
+    val items: List<Item>,
+    val bid: Bid
+) {
+    data class Item(
+        val id: String
+    )
+
+    data class Bid(
+        val requirementResponses: List<RequirementResponse>?,
+        val relatedLots: List<String>
+    ) {
+        data class RequirementResponse(
+            val id: String,
+            val title: String,
+
+            val description: String?,
+
+            val value: RequirementRsValue,
+            val requirement: Requirement,
+
+            val period: Period?
+        ) {
+            data class Requirement(
+                val id: String
+            )
+
+            data class Period(
+                val startDate: LocalDateTime,
+                val endDate: LocalDateTime
+            )
+        }
+    }
+}
