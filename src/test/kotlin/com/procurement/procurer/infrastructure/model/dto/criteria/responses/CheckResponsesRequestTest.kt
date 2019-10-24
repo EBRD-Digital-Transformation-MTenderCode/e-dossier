5f78bfef63d03a0b1dfc5a8ef040260d6cf4b041
Evgen Kopach <zkopach@gmail.com>
2019-10-24 19:21:24 +0300

    ES-2009 Created tests for request DTO ('checkResponses' command)

---
+++ src/test/kotlin/com/procurement/procurer/infrastructure/model/dto/criteria/responses/CheckResponsesRequestTest.kt
@@ -0,0 +1,28 @@
+package com.procurement.procurer.infrastructure.model.dto.criteria.responses
+
+import com.procurement.procurer.infrastructure.model.dto.AbstractDTOTestBase
+import com.procurement.procurer.infrastructure.model.dto.cn.CheckResponsesRequest
+import org.junit.jupiter.api.Test
+
+class CheckResponsesRequestTest : AbstractDTOTestBase<CheckResponsesRequest>(CheckResponsesRequest::class.java) {
+
+    @Test
+    fun fully() {
+        testBindingAndMapping("json/service/criteria/response/check/request/request_check_responses_full.json")
+    }
+
+    @Test
+    fun required_1() {
+        testBindingAndMapping("json/service/criteria/response/check/request/request_check_responses_required_1.json")
+    }
+
+    @Test
+    fun required_2() {
+        testBindingAndMapping("json/service/criteria/response/check/request/request_check_responses_required_2.json")
+    }
+
+    @Test
+    fun required_3() {
+        testBindingAndMapping("json/service/criteria/response/check/request/request_check_responses_required_3.json")
+    }
+}
