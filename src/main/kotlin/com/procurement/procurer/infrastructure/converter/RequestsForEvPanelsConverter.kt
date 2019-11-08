e6155ba482a5c7b65bf745deec9abed0aa7c1a8c
Evgen Kopach <zkopach@gmail.com>
2019-11-08 16:33:32 +0200

    ES-2161 Created converter (service-response) dto for 'createRequestsForEvPanels' command

---
+++ src/main/kotlin/com/procurement/procurer/infrastructure/converter/RequestsForEvPanelsConverter.kt
@@ -0,0 +1,31 @@
+package com.procurement.procurer.infrastructure.converter
+
+import com.procurement.procurer.application.model.data.RequestsForEvPanelsData
+import com.procurement.procurer.application.model.data.Requirement
+import com.procurement.procurer.infrastructure.model.dto.cn.RequestsForEvPanelsResponse
+
+fun RequestsForEvPanelsData.toResponseDto(): RequestsForEvPanelsResponse {
+    return RequestsForEvPanelsResponse(
+        criteria = RequestsForEvPanelsResponse.Criteria(
+            id = this.criteria.id,
+            title = this.criteria.title,
+            description = this.criteria.description,
+            source = this.criteria.source,
+            requirementGroups = this.criteria.requirementGroups.map { requirementGroup ->
+                RequestsForEvPanelsResponse.Criteria.RequirementGroup(
+                    id = requirementGroup.id,
+                    requirements = requirementGroup.requirements.map { requirement ->
+                        Requirement(
+                            id = requirement.id,
+                            title = requirement.title,
+                            dataType = requirement.dataType,
+                            value = requirement.value,
+                            period = requirement.period,
+                            description = requirement.description
+                        )
+                    }
+                )
+            }
+        )
+    )
+}
