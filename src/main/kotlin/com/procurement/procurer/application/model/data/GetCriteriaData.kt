f56a73661ecb0bb339b32ef3e1066a61f909a2df
Evgen Kopach <zkopach@gmail.com>
2019-11-08 14:58:43 +0200

    ES-2156 Created service dto for response dto at 'getCriteria' command

---
+++ src/main/kotlin/com/procurement/procurer/application/model/data/GetCriteriaData.kt
@@ -0,0 +1,28 @@
+package com.procurement.procurer.application.model.data
+
+import com.procurement.procurer.infrastructure.model.dto.ocds.AwardCriteria
+import com.procurement.procurer.infrastructure.model.dto.ocds.AwardCriteriaDetails
+import com.procurement.procurer.infrastructure.model.dto.ocds.ConversionsRelatesTo
+import java.math.BigDecimal
+
+data class GetCriteriaData(
+    val awardCriteria: AwardCriteria,
+    val awardCriteriaDetails: AwardCriteriaDetails,
+    val conversions: List<Conversion>?
+) {
+    data class Conversion(
+        val id: String,
+        val relatesTo: ConversionsRelatesTo,
+        val relatedItem: String,
+        val rationale: String,
+        val description: String?,
+
+        val coefficients: List<Coefficient>
+    ) {
+        data class Coefficient(
+            val id: String,
+            val value: CoefficientValue,
+            val coefficient: BigDecimal
+        )
+    }
+}
