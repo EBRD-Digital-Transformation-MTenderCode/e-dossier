5e61090fca3ac6cf070eee1622b135807a6dd17a
Evgen Kopach <zkopach@gmail.com>
2019-11-08 14:56:31 +0200

    ES-2156 Created tests for response dto at 'getCriteria' command

---
+++ src/test/kotlin/com/procurement/procurer/infrastructure/model/dto/criteria/get/GetCriteriaResponseTest.kt
@@ -0,0 +1,23 @@
+package com.procurement.procurer.infrastructure.model.dto.criteria.get
+
+import com.procurement.procurer.infrastructure.model.dto.AbstractDTOTestBase
+import com.procurement.procurer.infrastructure.model.dto.cn.GetCriteriaResponse
+import org.junit.jupiter.api.Test
+
+class GetCriteriaResponseTest : AbstractDTOTestBase<GetCriteriaResponse>(GetCriteriaResponse::class.java) {
+
+    @Test
+    fun fully() {
+        testBindingAndMapping("json/service/criteria/get/response_get_criteria_full.json")
+    }
+
+    @Test
+    fun required_1() {
+        testBindingAndMapping("json/service/criteria/get/response_get_criteria_required_1.json")
+    }
+
+    @Test
+    fun required_2() {
+        testBindingAndMapping("json/service/criteria/get/response_get_criteria_required_2.json")
+    }
+}
