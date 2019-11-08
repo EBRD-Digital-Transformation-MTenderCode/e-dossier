2761cb154dcaf772e04189b9e69a8c90226c237d
Evgen Kopach <zkopach@gmail.com>
2019-11-08 15:00:12 +0200

    ES-2156 Created converter for dto (service-response) at 'getCriteria' command

---
+++ src/main/kotlin/com/procurement/procurer/infrastructure/converter/GetCriteriaConverter.kt
@@ -0,0 +1,53 @@
+package com.procurement.procurer.infrastructure.converter
+
+import com.procurement.procurer.application.model.data.CreatedCriteria
+import com.procurement.procurer.application.model.data.GetCriteriaData
+import com.procurement.procurer.infrastructure.model.dto.cn.GetCriteriaResponse
+
+fun createResponseData(createdCriteria: CreatedCriteria): GetCriteriaData {
+    return GetCriteriaData(
+        conversions = createdCriteria.conversions?.map { conversion ->
+            GetCriteriaData.Conversion(
+                id = conversion.id,
+                relatesTo = conversion.relatesTo,
+                relatedItem = conversion.relatedItem,
+                description = conversion.description,
+                rationale = conversion.rationale,
+                coefficients = conversion.coefficients.map { coefficient ->
+                    GetCriteriaData.Conversion.Coefficient(
+                        id = coefficient.id,
+                        value = coefficient.value,
+                        coefficient = coefficient.coefficient
+                    )
+                }
+            )
+        },
+        awardCriteria = createdCriteria.awardCriteria,
+        awardCriteriaDetails = createdCriteria.awardCriteriaDetails!!
+    )
+}
+
+fun GetCriteriaData.toResponseDto(): GetCriteriaResponse {
+    return GetCriteriaResponse(
+        conversions = this.conversions?.map { conversion ->
+            GetCriteriaResponse.Conversion(
+                id = conversion.id,
+                relatesTo = conversion.relatesTo,
+                relatedItem = conversion.relatedItem,
+                description = conversion.description,
+                rationale = conversion.rationale,
+                coefficients = conversion.coefficients.map { coefficient ->
+                    GetCriteriaResponse.Conversion.Coefficient(
+                        id = coefficient.id,
+                        value = coefficient.value,
+                        coefficient = coefficient.coefficient
+                    )
+                }
+            )
+        },
+        awardCriteria = this.awardCriteria,
+        awardCriteriaDetails = this.awardCriteriaDetails
+    )
+}
+
+
