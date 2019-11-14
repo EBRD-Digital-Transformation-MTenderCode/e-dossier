a788fe515c749716df04ed9eeaaf2e40c73bf713
Evgen Kopach <zkopach@gmail.com>
2019-11-14 12:32:37 +0200

    ES-2191 Fixed uniqueness validation at 'checkCriteria' command

--- src/main/kotlin/com/procurement/procurer/application/exception/ErrorType.kt
+++ src/main/kotlin/com/procurement/procurer/application/exception/ErrorType.kt
@@ -12,5 +12,7 @@ enum class ErrorType constructor(val code: String, val message: String) {
     INVALID_AWARD_CRITERIA("10.70", "Invalid award criteria."),
     INVALID_JSON("10.71", "Invalid json."),
     ENTITY_NOT_FOUND("10.72", "No entity found in db with specified parameters."),
-    INVALID_REQUIREMENT_RESPONSE("10.73", "Invalid requirement response.");
+    INVALID_REQUIREMENT_RESPONSE("10.73", "Invalid requirement response."),
+    EMPTY_LIST("10.74", "Collection is empty."),
+    NOT_UNIQUE_IDS("10.75", "Elements in collection is not unique by id.");
 }
