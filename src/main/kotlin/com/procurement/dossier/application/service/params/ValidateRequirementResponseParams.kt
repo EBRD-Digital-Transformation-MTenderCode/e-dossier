f9bcbbae4bbaf3af3c0bf27abfd1feb198a023f9
Yevhenii Skoryi <yevheniiskoryi@gmail.com>
2020-03-20 17:29:49 +0200

    ES-2633 created application dto for command 'ValidateRequirementResponse'

---
+++ src/main/kotlin/com/procurement/dossier/application/service/params/ValidateRequirementResponseParams.kt
@@ -0,0 +1,55 @@
+package com.procurement.dossier.application.service.params
+
+import com.procurement.dossier.application.model.data.RequirementRsValue
+import com.procurement.dossier.domain.fail.error.DataErrors
+import com.procurement.dossier.domain.model.Cpid
+import com.procurement.dossier.domain.model.Ocid
+import com.procurement.dossier.domain.util.Result
+import com.procurement.dossier.domain.util.asFailure
+import com.procurement.dossier.domain.util.asSuccess
+
+class ValidateRequirementResponseParams private constructor(
+    val cpid: Cpid,
+    val id: String,
+    val ocid: Ocid,
+    val requirementId: String,
+    val value: RequirementRsValue
+) {
+    companion object {
+        fun tryCreate(
+            cpid: String,
+            id: String,
+            ocid: String,
+            requirementId: String,
+            value: RequirementRsValue
+        ): Result<ValidateRequirementResponseParams, DataErrors> {
+            val cpidResult = Cpid.tryCreate(cpid = cpid)
+                .doOnError { pattert ->
+                    return DataErrors.Validation.DataMismatchToPattern(
+                        actualValue = cpid,
+                        name = "cpid",
+                        pattern = pattert
+                    ).asFailure()
+                }
+                .get
+
+            val ocidResult = Ocid.tryCreate(ocid = ocid)
+                .doOnError { pattert ->
+                    return DataErrors.Validation.DataMismatchToPattern(
+                        actualValue = ocid,
+                        name = "ocid",
+                        pattern = pattert
+                    ).asFailure()
+                }
+                .get
+
+            return ValidateRequirementResponseParams(
+                id = id,
+                cpid = cpidResult,
+                ocid = ocidResult,
+                requirementId = requirementId,
+                value = value
+            ).asSuccess()
+        }
+    }
+}
