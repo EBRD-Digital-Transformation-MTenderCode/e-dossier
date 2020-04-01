518f10f68e629b4ab876441ab38818780952f4d8
Evgen Kopach <zkopach@gmail.com>
2020-04-01 11:55:07 +0300

    ES-2781 Changed object mapper configuration

--- src/main/kotlin/com/procurement/dossier/infrastructure/config/ObjectMapperConfiguration.kt
+++ src/main/kotlin/com/procurement/dossier/infrastructure/config/ObjectMapperConfiguration.kt
@@ -1,13 +1,15 @@
 package com.procurement.dossier.infrastructure.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.procurement.dossier.infrastructure.bind.jackson.configuration
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class ObjectMapperConfiguration(objectMapper: ObjectMapper) {
+class ObjectMapperConfiguration {
 
-    init {
-        objectMapper.configuration()
-    }
+    @Bean
+    fun objectMapper(): ObjectMapper = jacksonObjectMapper().apply { configuration() }
+
 }
