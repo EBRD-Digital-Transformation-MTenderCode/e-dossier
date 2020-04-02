7a0f50f209b6815906f819da0147618f53135899
katherine <kate.shaidurova@gmail.com>
2020-04-02 12:23:45 +0300

    ES-2794 Added parseRequirementId and parseRequirementResponseId method to ParseParams

--- src/main/kotlin/com/procurement/dossier/application/model/ParseParams.kt
+++ src/main/kotlin/com/procurement/dossier/application/model/ParseParams.kt
@@ -3,6 +3,9 @@ package com.procurement.dossier.application.model
 import com.procurement.dossier.domain.fail.error.DataErrors
 import com.procurement.dossier.domain.model.Cpid
 import com.procurement.dossier.domain.model.Ocid
+import com.procurement.dossier.domain.model.requirement.RequirementId
+import com.procurement.dossier.domain.model.requirement.response.tryRequirementResponseId
+import com.procurement.dossier.domain.model.requirement.tryRequirementId
 import com.procurement.dossier.domain.util.Result
 import com.procurement.dossier.domain.util.asSuccess
 
@@ -33,3 +36,28 @@ fun parseOcid(value: String): Result<Ocid, DataErrors.Validation.DataMismatchToP
         }
         .get
         .asSuccess()
+
+fun parseRequirementId(id: String): Result<RequirementId, DataErrors> =
+    id.tryRequirementId()
+        .doReturn {
+            return Result.failure(
+                DataErrors.Validation.DataFormatMismatch(
+                    name = "requirementId",
+                    expectedFormat = "string",
+                    actualValue = id
+                )
+            )
+        }.asSuccess()
+
+fun parseRequirementResponseId(id: String): Result<RequirementId, DataErrors> =
+    id.tryRequirementResponseId()
+        .doReturn {
+            return Result.failure(
+                DataErrors.Validation.DataFormatMismatch(
+                    name = "requirementResponseId",
+                    expectedFormat = "string",
+                    actualValue = id
+                )
+            )
+        }.asSuccess()
+
