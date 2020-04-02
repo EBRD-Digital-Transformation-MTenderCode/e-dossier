25dd2df3989f8a75e118383dd4640ad4242c9cdb
katherine <kate.shaidurova@gmail.com>
2020-04-02 12:22:52 +0300

    ES-2794 Added RequirementId and RequirementResponseId typealiases

---
+++ src/main/kotlin/com/procurement/dossier/domain/model/requirement/RequirementId.kt
@@ -0,0 +1,8 @@
+package com.procurement.dossier.domain.model.requirement
+import com.procurement.dossier.domain.fail.Fail
+import com.procurement.dossier.domain.util.Result
+
+typealias RequirementId = String
+
+fun String.tryRequirementId(): Result<RequirementId, Fail.Incident.Transforming> =
+    Result.success(this)
