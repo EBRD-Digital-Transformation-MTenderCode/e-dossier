f7a1c6f91775d52055362acb140a1f7d8cf67183
katherine <kate.shaidurova@gmail.com>
2020-05-15 18:17:00 +0300

    ES-3267 Added VALIDATE_PERIOD branch to CommandService.kt and GPA to pmd checks

--- src/main/kotlin/com/procurement/dossier/infrastructure/service/CommandService.kt
+++ src/main/kotlin/com/procurement/dossier/infrastructure/service/CommandService.kt
@@ -4,27 +4,34 @@ import com.procurement.dossier.application.exception.ErrorException
 import com.procurement.dossier.application.exception.ErrorType
 import com.procurement.dossier.application.model.data.CreatedCriteria
 import com.procurement.dossier.application.model.data.GetCriteriaData
+import com.procurement.dossier.application.model.data.period.validate.ValidatePeriodContext
 import com.procurement.dossier.application.service.CriteriaService
+import com.procurement.dossier.application.service.PeriodService
 import com.procurement.dossier.application.service.command.generateCreateCriteriaResponse
 import com.procurement.dossier.application.service.context.CheckResponsesContext
 import com.procurement.dossier.application.service.context.CreateCriteriaContext
 import com.procurement.dossier.application.service.context.EvPanelsContext
 import com.procurement.dossier.application.service.context.GetCriteriaContext
+import com.procurement.dossier.infrastructure.converter.period.convert
 import com.procurement.dossier.infrastructure.converter.toResponseDto
 import com.procurement.dossier.infrastructure.dto.ApiSuccessResponse
 import com.procurement.dossier.infrastructure.model.dto.bpe.CommandMessage
 import com.procurement.dossier.infrastructure.model.dto.bpe.CommandType
+import com.procurement.dossier.infrastructure.model.dto.bpe.country
 import com.procurement.dossier.infrastructure.model.dto.bpe.cpid
 import com.procurement.dossier.infrastructure.model.dto.bpe.owner
 import com.procurement.dossier.infrastructure.model.dto.bpe.pmd
 import com.procurement.dossier.infrastructure.model.dto.ocds.ProcurementMethod
+import com.procurement.dossier.infrastructure.model.dto.request.period.ValidatePeriodRequest
 import com.procurement.dossier.infrastructure.utils.toJson
+import com.procurement.dossier.infrastructure.utils.toObject
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class CommandService(
-    private val criteriaService: CriteriaService
+    private val criteriaService: CriteriaService,
+    private val periodService: PeriodService
 ) {
     companion object {
         private val log = LoggerFactory.getLogger(CommandService::class.java)
@@ -47,7 +54,8 @@ class CommandService(
                     ProcurementMethod.FA, ProcurementMethod.TEST_FA,
                     ProcurementMethod.DA, ProcurementMethod.TEST_DA,
                     ProcurementMethod.NP, ProcurementMethod.TEST_NP,
-                    ProcurementMethod.OP, ProcurementMethod.TEST_OP -> {
+                    ProcurementMethod.OP, ProcurementMethod.TEST_OP,
+                    ProcurementMethod.GPA, ProcurementMethod.TEST_GPA -> {
                         throw ErrorException(ErrorType.INVALID_PMD)
                     }
                 }
@@ -72,7 +80,8 @@ class CommandService(
                     ProcurementMethod.FA, ProcurementMethod.TEST_FA,
                     ProcurementMethod.DA, ProcurementMethod.TEST_DA,
                     ProcurementMethod.NP, ProcurementMethod.TEST_NP,
-                    ProcurementMethod.OP, ProcurementMethod.TEST_OP -> {
+                    ProcurementMethod.OP, ProcurementMethod.TEST_OP,
+                    ProcurementMethod.GPA, ProcurementMethod.TEST_GPA -> {
                         throw ErrorException(ErrorType.INVALID_PMD)
                     }
 
@@ -94,7 +103,8 @@ class CommandService(
                     ProcurementMethod.FA, ProcurementMethod.TEST_FA,
                     ProcurementMethod.DA, ProcurementMethod.TEST_DA,
                     ProcurementMethod.NP, ProcurementMethod.TEST_NP,
-                    ProcurementMethod.OP, ProcurementMethod.TEST_OP -> {
+                    ProcurementMethod.OP, ProcurementMethod.TEST_OP,
+                    ProcurementMethod.GPA, ProcurementMethod.TEST_GPA -> {
                         throw ErrorException(ErrorType.INVALID_PMD)
                     }
 
@@ -121,7 +131,8 @@ class CommandService(
                     ProcurementMethod.FA, ProcurementMethod.TEST_FA,
                     ProcurementMethod.DA, ProcurementMethod.TEST_DA,
                     ProcurementMethod.NP, ProcurementMethod.TEST_NP,
-                    ProcurementMethod.OP, ProcurementMethod.TEST_OP -> {
+                    ProcurementMethod.OP, ProcurementMethod.TEST_OP,
+                    ProcurementMethod.GPA, ProcurementMethod.TEST_GPA -> {
                         throw ErrorException(ErrorType.INVALID_PMD)
                     }
 
@@ -131,7 +142,8 @@ class CommandService(
                 when (cm.pmd) {
                     ProcurementMethod.OT, ProcurementMethod.TEST_OT,
                     ProcurementMethod.SV, ProcurementMethod.TEST_SV,
-                    ProcurementMethod.MV, ProcurementMethod.TEST_MV -> {
+                    ProcurementMethod.MV, ProcurementMethod.TEST_MV,
+                    ProcurementMethod.GPA, ProcurementMethod.TEST_GPA -> {
                         val context = EvPanelsContext(cpid = cm.cpid, owner = cm.owner)
                         criteriaService.createRequestsForEvPanels(context = context)
                             .also { result ->
@@ -149,6 +161,30 @@ class CommandService(
                         throw ErrorException(ErrorType.INVALID_PMD)
                     }
 
+                }
+            }
+            CommandType.VALIDATE_PERIOD -> {
+                when (cm.pmd) {
+                    ProcurementMethod.GPA, ProcurementMethod.TEST_GPA -> {
+                        val context = ValidatePeriodContext(country = cm.country, pmd = cm.pmd)
+                        val data = toObject(ValidatePeriodRequest::class.java, cm.data).convert()
+                        periodService.validatePeriod(data = data, context = context)
+                            .also {
+                                if (log.isDebugEnabled)
+                                    log.debug("Period validation completed successfully")
+                            }
+                    }
+                    ProcurementMethod.OT, ProcurementMethod.TEST_OT,
+                    ProcurementMethod.SV, ProcurementMethod.TEST_SV,
+                    ProcurementMethod.MV, ProcurementMethod.TEST_MV,
+                    ProcurementMethod.RT, ProcurementMethod.TEST_RT,
+                    ProcurementMethod.FA, ProcurementMethod.TEST_FA,
+                    ProcurementMethod.DA, ProcurementMethod.TEST_DA,
+                    ProcurementMethod.NP, ProcurementMethod.TEST_NP,
+                    ProcurementMethod.OP, ProcurementMethod.TEST_OP
+                    -> {
+                        throw ErrorException(ErrorType.INVALID_PMD)
+                    }
                 }
             }
         }
