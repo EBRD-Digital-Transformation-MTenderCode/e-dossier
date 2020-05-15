3b4d391385d0342449b15c1e2527d18dcf2e25ca
katherine <kate.shaidurova@gmail.com>
2020-05-15 18:13:10 +0300

    ES-3267 Added sql for period and period_rules tables generation

--- docs/data.sql
+++ docs/data.sql
@@ -11,3 +11,20 @@ CREATE TABLE IF NOT EXISTS dossier.tenders
     json_data text,
     primary key (cp_id)
 );
+
+CREATE TABLE IF NOT EXISTS  dossier.period (
+    cpid text,
+    ocid text,
+    start_date timestamp,
+    end_date timestamp,
+    primary key(cpid, ocid)
+);
+
+CREATE TABLE IF NOT EXISTS  dossier.period_rules (
+    country text,
+    pmd text,
+    value bigint,
+    primary key(country, pmd)
+);
+
+INSERT INTO dossier.period_rules (country, pmd, value)  VALUES ('MD', 'GPA', 864000);
