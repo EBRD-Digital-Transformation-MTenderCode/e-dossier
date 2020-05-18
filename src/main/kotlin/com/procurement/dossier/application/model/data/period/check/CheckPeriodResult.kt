2f41cf8872185d0c45d7149c76710aa54e1f991f
katherine <kate.shaidurova@gmail.com>
2020-05-18 16:34:08 +0300

    ES-3269 Added CheckPeriodResult.kt

---
+++ src/main/kotlin/com/procurement/dossier/application/model/data/period/check/CheckPeriodResult.kt
@@ -0,0 +1,18 @@
+package com.procurement.dossier.application.model.data.period.check
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import java.time.LocalDateTime
+
+data class CheckPeriodResult(
+    @param:JsonProperty("isPreQualificationPeriodChanged") @get:JsonProperty("isPreQualificationPeriodChanged") val isPreQualificationPeriodChanged: Boolean,
+    @param:JsonProperty("preQualification") @field:JsonProperty("preQualification") val preQualification: PreQualification
+) {
+    data class PreQualification(
+        @param:JsonProperty("period") @field:JsonProperty("period") val period: Period
+    ) {
+        data class Period(
+            @param:JsonProperty("startDate") @field:JsonProperty("startDate") val startDate: LocalDateTime,
+            @param:JsonProperty("endDate") @field:JsonProperty("endDate") val endDate: LocalDateTime
+        )
+    }
+}
