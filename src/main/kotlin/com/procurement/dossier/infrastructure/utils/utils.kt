abafb6238ca4c45f3e894a413ab6050b0c14e859
katherine <kate.shaidurova@gmail.com>
2020-05-21 17:00:04 +0300

    ES-3242 Added tryToJson() to utils.kt

--- src/main/kotlin/com/procurement/dossier/infrastructure/utils/utils.kt
+++ src/main/kotlin/com/procurement/dossier/infrastructure/utils/utils.kt
@@ -9,16 +9,15 @@ import com.fasterxml.jackson.databind.node.JsonNodeFactory
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.procurement.dossier.domain.fail.Fail
 import com.procurement.dossier.domain.util.Result
+import com.procurement.dossier.domain.util.asSuccess
 import com.procurement.dossier.infrastructure.bind.databinding.IntDeserializer
 import com.procurement.dossier.infrastructure.bind.databinding.JsonDateTimeDeserializer
-import com.procurement.dossier.infrastructure.bind.databinding.JsonDateTimeFormatter
 import com.procurement.dossier.infrastructure.bind.databinding.JsonDateTimeSerializer
 import com.procurement.dossier.infrastructure.bind.databinding.StringsDeserializer
 import java.io.IOException
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneOffset
-import java.util.*
 
 private object JsonMapper {
 
@@ -69,6 +68,13 @@ fun <Any> toJson(obj: Any): String {
         throw RuntimeException(e)
     }
 }
+
+fun <Any> tryToJson(obj: Any): Result<String, Fail.Incident.Parsing>  =
+    try {
+        JsonMapper.mapper.writeValueAsString(obj).asSuccess()
+    } catch (expected: JsonProcessingException) {
+        Result.failure(Fail.Incident.Parsing(className = String::class.java.canonicalName, exception = expected))
+    }
 
 fun <T> toObject(clazz: Class<T>, json: String): T {
     try {
