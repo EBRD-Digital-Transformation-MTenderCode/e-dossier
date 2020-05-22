8389737d9400cbb1a24385487ce24352d72b6062
katherine <kate.shaidurova@gmail.com>
2020-05-22 16:08:30 +0300

    ES-3261 Added checkPeriod2() to PeriodService.kt

--- src/main/kotlin/com/procurement/dossier/application/service/PeriodService.kt
+++ src/main/kotlin/com/procurement/dossier/application/service/PeriodService.kt
@@ -5,12 +5,16 @@ import com.procurement.dossier.application.exception.ErrorType
 import com.procurement.dossier.application.model.data.period.check.CheckPeriodContext
 import com.procurement.dossier.application.model.data.period.check.CheckPeriodData
 import com.procurement.dossier.application.model.data.period.check.CheckPeriodResult
+import com.procurement.dossier.application.model.data.period.check.params.CheckPeriod2Params
 import com.procurement.dossier.application.model.data.period.save.SavePeriodContext
 import com.procurement.dossier.application.model.data.period.save.SavePeriodData
 import com.procurement.dossier.application.model.data.period.validate.ValidatePeriodContext
 import com.procurement.dossier.application.model.data.period.validate.ValidatePeriodData
 import com.procurement.dossier.application.repository.PeriodRepository
 import com.procurement.dossier.application.repository.PeriodRulesRepository
+import com.procurement.dossier.domain.fail.Fail
+import com.procurement.dossier.domain.fail.error.ValidationErrors
+import com.procurement.dossier.domain.util.ValidationResult
 import com.procurement.dossier.infrastructure.model.entity.PeriodEntity
 import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
@@ -79,4 +83,19 @@ class PeriodService(
                 startDate = data.period.startDate
             )
         )
+
+    fun checkPeriod2(params: CheckPeriod2Params): ValidationResult<Fail> {
+        val storedPeriod = periodRepository.tryFindBy(cpid = params.cpid, ocid = params.ocid)
+            .doReturn { incident -> return ValidationResult.error(incident) }!!
+
+        val requestDate = params.date
+
+        if (!requestDate.isAfter(storedPeriod.startDate))
+            return ValidationResult.error(ValidationErrors.InvalidPeriodDateComparedWithStartDate())
+
+        if (!requestDate.isBefore(storedPeriod.endDate))
+            return ValidationResult.error(ValidationErrors.InvalidPeriodDateComparedWithEndDate())
+
+        return ValidationResult.ok()
+    }
 }
