fd3455af15aeb75f4b80360bb5783d966bbe62ce
katherine <kate.shaidurova@gmail.com>
2020-05-22 16:01:34 +0300

    ES-3261 Rewrote incidents removing duplications

--- src/main/kotlin/com/procurement/dossier/domain/fail/Fail.kt
+++ src/main/kotlin/com/procurement/dossier/domain/fail/Fail.kt
@@ -1,6 +1,5 @@
 package com.procurement.dossier.domain.fail
 
-import com.fasterxml.jackson.annotation.JsonValue
 import com.procurement.dossier.application.service.Logger
 import com.procurement.dossier.domain.EnumElementProvider
 import com.procurement.dossier.domain.util.Result
@@ -8,30 +7,26 @@ import com.procurement.dossier.domain.util.ValidationResult
 
 sealed class Fail {
 
+    abstract val code: String
+    abstract val description: String
+    val message: String
+        get() = "ERROR CODE: '$code', DESCRIPTION: '$description'."
+
     abstract fun logging(logger: Logger)
 
     abstract class Error(val prefix: String) : Fail() {
-        abstract val code: String
-        abstract val description: String
-        val message: String
-            get() = "ERROR CODE: '$code', DESCRIPTION: '$description'."
+        companion object {
+            fun <T, E : Error> E.toResult(): Result<T, E> = Result.failure(this)
+            fun <E : Error> E.toValidationResult(): ValidationResult<E> = ValidationResult.error(this)
+        }
 
         override fun logging(logger: Logger) {
             logger.error(message = message)
         }
-
-        companion object {
-            fun <T, E : Error> E.toResult(): Result<T, E> = Result.failure(this)
-
-            fun <E : Error> E.toValidationResult(): ValidationResult<E> = ValidationResult.error(this)
-        }
     }
 
-    sealed class Incident(val level: Level, number: String, val description: String) : Fail() {
-        val code: String = "INC-$number"
-
-        val message: String
-            get() = "INCIDENT CODE: '$code', DESCRIPTION: '$description'."
+    sealed class Incident(val level: Level, number: String, override val description: String) : Fail() {
+        override val code: String = "INC-$number"
 
         override fun logging(logger: Logger) {
             when (level) {
@@ -41,51 +36,61 @@ sealed class Fail {
             }
         }
 
-        class Database(val exception: Exception) :
-            Incident(
-                level = Level.ERROR,
-                number = "01",
+        sealed class Database(val number: String, override val description: String) :
+            Incident(level = Level.ERROR, number = number, description = description) {
+
+            class DatabaseInteractionIncident(private val exception: Exception) : Database(
+                number = "1.1",
                 description = "Database incident."
             ) {
-            override fun logging(logger: Logger) {
-                logger.error(message = message, exception = exception)
+                override fun logging(logger: Logger) {
+                    logger.error(message = message, exception = exception)
+                }
             }
-        }
 
-        class Parsing(val className: String, val exception: Exception) :
-            Incident(
+            class RecordDoesNotExist(override val description: String) : Database(
+                number = "1.2",
+                description = description
+            )
+
+            class DatabaseConsistencyIncident(message: String) : Incident(
                 level = Level.ERROR,
-                number = "02",
-                description = "Error parsing to class '$className'."
-            ) {
+                number = "1.3",
+                description = "Database consistency incident. $message"
+            )
+        }
+
+        sealed class Transform(val number: String, override val description: String, val exception: Exception? = null) :
+            Incident(level = Level.ERROR, number = number, description = description) {
+
             override fun logging(logger: Logger) {
                 logger.error(message = message, exception = exception)
             }
-        }
 
-        class Transforming(val exception: Exception) :
-            Incident(
-                level = Level.ERROR,
-                number = "03",
-                description = "Error transforming."
+            class ParseFromDatabaseIncident(val jsonData: String, exception: Exception? = null) : Transform(
+                number = "2.1",
+                description = "Could not parse data stored in database.",
+                exception = exception
             ) {
-            override fun logging(logger: Logger) {
-                logger.error(message = message, exception = exception)
+                override fun logging(logger: Logger) {
+                    logger.error(message = message, mdc = mapOf("jsonData" to jsonData), exception = exception)
+                }
+            }
+
+            class Parsing(className: String, exception: Exception? = null) :
+                Transform(number = "2.2", description = "Error parsing to $className.", exception = exception)
+
+            class ParseFromDatabaseColumnIncident(val column: String, val value: String) : Transform(
+                number = "2.3",
+                description = "Could not parse data stored in database."
+            ) {
+                override fun logging(logger: Logger) {
+                    logger.error(message = message, mdc = mapOf("column" to column, "value" to value))
+                }
             }
         }
 
-        class DatabaseIncident(val exception: Exception? = null) :
-            Incident(
-                level = Level.ERROR,
-                number = "04",
-                description = "Internal Server Error."
-            ) {
-            override fun logging(logger: Logger) {
-                logger.error(message = message, exception = exception)
-            }
-        }
-
-        enum class Level(@JsonValue override val key: String) : EnumElementProvider.Key {
+        enum class Level(override val key: String) : EnumElementProvider.Key {
             ERROR("error"),
             WARNING("warning"),
             INFO("info");
@@ -94,7 +99,3 @@ sealed class Fail {
         }
     }
 }
-
-fun <T, E : Fail.Error> E.toResult(): Result<T, E> = Result.failure(this)
-
-fun <E : Fail.Error> E.toValidationResult(): ValidationResult<E> = ValidationResult.error(this)
