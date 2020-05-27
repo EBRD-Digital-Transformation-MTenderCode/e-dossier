5fb98c20420dabe3a84525baac6063065c2faca4
katherine <kate.shaidurova@gmail.com>
2020-05-27 21:55:38 +0300

    ES-3235 Made SubmissionState a data class

--- src/main/kotlin/com/procurement/dossier/domain/model/submission/SubmissionState.kt
+++ src/main/kotlin/com/procurement/dossier/domain/model/submission/SubmissionState.kt
@@ -2,7 +2,7 @@ package com.procurement.dossier.domain.model.submission
 
 import com.procurement.dossier.domain.model.enums.SubmissionStatus
 
-class SubmissionState(
+data class SubmissionState(
     val id: SubmissionId,
     val status: SubmissionStatus
 )
