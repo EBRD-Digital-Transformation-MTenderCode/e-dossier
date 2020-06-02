15f56c3d52403b0b28a7f608229f9ed3319eb767
katherine <kate.shaidurova@gmail.com>
2020-06-02 17:51:35 +0300

    ES-3342 Removed redundant imports

--- src/test/kotlin/com/procurement/dossier/infrastructure/model/dto/submission/find/FindSubmissionsForOpeningRequestTest.kt
+++ src/test/kotlin/com/procurement/dossier/infrastructure/model/dto/submission/find/FindSubmissionsForOpeningRequestTest.kt
@@ -1,11 +1,12 @@
 package com.procurement.dossier.infrastructure.model.dto.submission.find
 
 import com.procurement.dossier.infrastructure.model.dto.AbstractDTOTestBase
-import com.procurement.dossier.infrastructure.model.dto.request.submission.CreateSubmissionRequest
 import com.procurement.dossier.infrastructure.model.dto.request.submission.FindSubmissionsForOpeningRequest
 import org.junit.jupiter.api.Test
 
-internal class FindSubmissionsForOpeningRequestTest : AbstractDTOTestBase<FindSubmissionsForOpeningRequest>(FindSubmissionsForOpeningRequest::class.java) {
+internal class FindSubmissionsForOpeningRequestTest : AbstractDTOTestBase<FindSubmissionsForOpeningRequest>(
+    FindSubmissionsForOpeningRequest::class.java
+) {
 
     @Test
     fun fully() {
--- src/test/kotlin/com/procurement/dossier/infrastructure/model/dto/submission/find/FindSubmissionsForOpeningResultTest.kt
+++ src/test/kotlin/com/procurement/dossier/infrastructure/model/dto/submission/find/FindSubmissionsForOpeningResultTest.kt
@@ -2,11 +2,11 @@ package com.procurement.dossier.infrastructure.model.dto.submission.find
 
 import com.procurement.dossier.application.model.data.submission.find.FindSubmissionsForOpeningResult
 import com.procurement.dossier.infrastructure.model.dto.AbstractDTOTestBase
-import com.procurement.dossier.infrastructure.model.dto.request.submission.CreateSubmissionRequest
-import com.procurement.dossier.infrastructure.model.dto.request.submission.FindSubmissionsForOpeningRequest
 import org.junit.jupiter.api.Test
 
-internal class FindSubmissionsForOpeningResultTest : AbstractDTOTestBase<FindSubmissionsForOpeningResult>(FindSubmissionsForOpeningResult::class.java) {
+internal class FindSubmissionsForOpeningResultTest : AbstractDTOTestBase<FindSubmissionsForOpeningResult>(
+    FindSubmissionsForOpeningResult::class.java
+) {
 
     @Test
     fun fully() {
