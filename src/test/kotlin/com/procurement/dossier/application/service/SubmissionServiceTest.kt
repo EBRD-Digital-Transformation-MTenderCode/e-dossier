cc034ac51421add598490c49529daa371bae32cd
katherine <kate.shaidurova@gmail.com>
2020-06-05 15:04:25 +0300

    ES-3239 Fixed SetStateForSubmission tests

--- src/test/kotlin/com/procurement/dossier/application/service/SubmissionServiceTest.kt
+++ src/test/kotlin/com/procurement/dossier/application/service/SubmissionServiceTest.kt
@@ -1,7 +1,10 @@
 package com.procurement.dossier.application.service
 
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.eq
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.whenever
+import com.procurement.dossier.application.model.data.RequirementRsValue
 import com.procurement.dossier.application.model.data.submission.check.CheckAccessToSubmissionParams
 import com.procurement.dossier.application.model.data.submission.state.get.GetSubmissionStateByIdsParams
 import com.procurement.dossier.application.model.data.submission.state.get.GetSubmissionStateByIdsResult
@@ -12,16 +15,25 @@ import com.procurement.dossier.domain.fail.Fail
 import com.procurement.dossier.domain.fail.error.ValidationErrors
 import com.procurement.dossier.domain.model.Cpid
 import com.procurement.dossier.domain.model.Ocid
+import com.procurement.dossier.domain.model.enums.BusinessFunctionType
+import com.procurement.dossier.domain.model.enums.DocumentType
+import com.procurement.dossier.domain.model.enums.PersonTitle
+import com.procurement.dossier.domain.model.enums.Scale
 import com.procurement.dossier.domain.model.enums.SubmissionStatus
+import com.procurement.dossier.domain.model.enums.SupplierType
+import com.procurement.dossier.domain.model.submission.Submission
 import com.procurement.dossier.domain.model.submission.SubmissionCredentials
 import com.procurement.dossier.domain.model.submission.SubmissionState
 import com.procurement.dossier.domain.util.Result
 import com.procurement.dossier.domain.util.ValidationResult
 import com.procurement.dossier.domain.util.asSuccess
+import com.procurement.dossier.infrastructure.bind.databinding.JsonDateTimeDeserializer
+import com.procurement.dossier.infrastructure.bind.databinding.JsonDateTimeSerializer
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
 import java.util.*
 
 internal class SubmissionServiceTest {
@@ -116,15 +128,27 @@ internal class SubmissionServiceTest {
         @Test
         fun success() {
             val params = getParams()
-            val submission = params.submission
+            val requestSubmission = params.submission
+            val storedSubmission = stubSubmission().copy(status = SubmissionStatus.WITHDRAWN)
+
             whenever(
-                submissionRepository.setSubmissionStatus(
-                    cpid = params.cpid, ocid = params.ocid, status = submission.status, id = submission.id
+                submissionRepository.findSubmission(
+                    cpid = params.cpid,
+                    ocid = params.ocid,
+                    id = requestSubmission.id
+                )
+            ).thenReturn(storedSubmission.asSuccess())
+
+            whenever(
+                submissionRepository.updateSubmission(
+                    cpid = eq(params.cpid),
+                    ocid = eq(params.ocid),
+                    submission = any()
                 )
             ).thenReturn(true.asSuccess())
 
             val actual = submissionService.setStateForSubmission(params = params).get
-            val expected = SetStateForSubmissionResult(id = submission.id, status = submission.status)
+            val expected = SetStateForSubmissionResult(id = requestSubmission.id, status = requestSubmission.status)
 
             assertEquals(actual, expected)
         }
@@ -132,12 +156,14 @@ internal class SubmissionServiceTest {
         @Test
         fun submissionNotFound_fail() {
             val params = getParams()
-            val submission = params.submission
+            val requestSubmission = params.submission
             whenever(
-                submissionRepository.setSubmissionStatus(
-                    cpid = params.cpid, ocid = params.ocid, status = submission.status, id = submission.id
+                submissionRepository.findSubmission(
+                    cpid = params.cpid,
+                    ocid = params.ocid,
+                    id = requestSubmission.id
                 )
-            ).thenReturn(false.asSuccess())
+            ).thenReturn(null.asSuccess())
 
             val actual = submissionService.setStateForSubmission(params = params).error
 
@@ -147,15 +173,17 @@ internal class SubmissionServiceTest {
         @Test
         fun databaseIncident_fail() {
             val params = getParams()
-            val submission = params.submission
+            val requestSubmission = params.submission
             val expected = Result.failure(
                 Fail.Incident.Database.Interaction(
                     exception = RuntimeException("")
                 )
             )
             whenever(
-                submissionRepository.setSubmissionStatus(
-                    cpid = params.cpid, ocid = params.ocid, status = submission.status, id = submission.id
+                submissionRepository.findSubmission(
+                    cpid = params.cpid,
+                    ocid = params.ocid,
+                    id = requestSubmission.id
                 )
             ).thenReturn(expected)
 
@@ -245,4 +273,176 @@ internal class SubmissionServiceTest {
                 token = TOKEN.toString()
             ).get
     }
+
+    private fun stubSubmission() =
+        Submission(
+            id = UUID.randomUUID(),
+            status = SubmissionStatus.PENDING,
+            owner = UUID.randomUUID(),
+            token = UUID.randomUUID(),
+            date = JsonDateTimeDeserializer.deserialize(JsonDateTimeSerializer.serialize(LocalDateTime.now())),
+            documents = listOf(
+                Submission.Document(
+                    documentType = DocumentType.REGULATORY_DOCUMENT,
+                    id = "document.id",
+                    description = "document.description",
+                    title = "document.title"
+                )
+            ),
+            requirementResponses = listOf(
+                Submission.RequirementResponse(
+                    id = "requirementResponse.id",
+                    value = RequirementRsValue.AsString("requirementResponse.value"),
+                    requirement = Submission.RequirementResponse.Requirement(id = "requirementResponse.requirement.id"),
+                    relatedCandidate = Submission.RequirementResponse.RelatedCandidate(
+                        id = "relatedCandidate.id",
+                        name = "relatedCandidate.name"
+                    )
+                )
+            ),
+            candidates = listOf(
+                Submission.Candidate(
+                    id = "candidate.id",
+                    name = "candidate.name",
+                    identifier = Submission.Candidate.Identifier(
+                        id = "identifier.id",
+                        scheme = "identifier.scheme",
+                        uri = "identifier.uri",
+                        legalName = "identifier.legalName"
+                    ),
+                    additionalIdentifiers = listOf(
+                        Submission.Candidate.AdditionalIdentifier(
+                            id = "additionalIdentifier.id",
+                            scheme = "additionalIdentifier.scheme",
+                            uri = "additionalIdentifier.uri",
+                            legalName = "additionalIdentifier.legalName"
+                        )
+                    ),
+                    persones = listOf(
+                        Submission.Candidate.Person(
+                            id = "person.id",
+                            title = PersonTitle.MR,
+                            identifier = Submission.Candidate.Person.Identifier(
+                                id = "persones.identifier.id",
+                                scheme = "persones.identifier.scheme",
+                                uri = "persones.identifier.uri"
+                            ),
+                            name = "persones.name",
+                            businessFunctions = listOf(
+                                Submission.Candidate.Person.BusinessFunction(
+                                    id = "businessFunction.id",
+                                    type = BusinessFunctionType.CHAIRMAN,
+                                    documents = listOf(
+                                        Submission.Candidate.Person.BusinessFunction.Document(
+                                            documentType = DocumentType.REGULATORY_DOCUMENT,
+                                            id = "businessFunctions.document.id",
+                                            description = "businessFunctions.document.description",
+                                            title = "businessFunctions.document.title"
+                                        )
+                                    ),
+                                    jobTitle = "jobTitle",
+                                    period = Submission.Candidate.Person.BusinessFunction.Period(
+                                        startDate = JsonDateTimeDeserializer.deserialize(
+                                            JsonDateTimeSerializer.serialize(
+                                                LocalDateTime.now()
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    ),
+                    address = Submission.Candidate.Address(
+                        streetAddress = "streetAddress",
+                        postalCode = "postalCode",
+                        addressDetails = Submission.Candidate.Address.AddressDetails(
+                            country = Submission.Candidate.Address.AddressDetails.Country(
+                                id = "country.id",
+                                scheme = "country.scheme",
+                                description = "country.description",
+                                uri = "country.uri"
+                            ),
+                            region = Submission.Candidate.Address.AddressDetails.Region(
+                                id = "region.id",
+                                scheme = "region.scheme",
+                                description = "region.description",
+                                uri = "region.uri"
+                            ),
+                            locality = Submission.Candidate.Address.AddressDetails.Locality(
+                                id = "locality.id",
+                                scheme = "locality.scheme",
+                                description = "locality.description",
+                                uri = "locality.uri"
+                            )
+                        )
+                    ),
+                    contactPoint = Submission.Candidate.ContactPoint(
+                        name = "contactPoint.name",
+                        url = "contactPoint.url",
+                        telephone = "contactPoint.telephone",
+                        faxNumber = "contactPoint.faxNumber",
+                        email = "contactPoint.email"
+                    ),
+                    details = Submission.Candidate.Details(
+                        typeOfSupplier = SupplierType.COMPANY,
+                        scale = Scale.LARGE,
+                        mainEconomicActivities = listOf(
+                            Submission.Candidate.Details.MainEconomicActivity(
+                                id = "mainEconomicActivities.id",
+                                scheme = "mainEconomicActivities.scheme",
+                                description = "mainEconomicActivities.description",
+                                uri = "mainEconomicActivities.uri"
+                            )
+                        ),
+                        legalForm = Submission.Candidate.Details.LegalForm(
+                            id = "legalForm.id",
+                            scheme = "legalForm.scheme",
+                            description = "legalForm.description",
+                            uri = "legalForm.uri"
+                        ),
+                        bankAccounts = listOf(
+                            Submission.Candidate.Details.BankAccount(
+                                description = "legalForm.bankAccounts",
+                                identifier = Submission.Candidate.Details.BankAccount.Identifier(
+                                    id = "bankAccounts.identifier.id",
+                                    scheme = "bankAccounts.identifier.scheme"
+                                ),
+                                bankName = "bankName",
+                                additionalAccountIdentifiers = listOf(
+                                    Submission.Candidate.Details.BankAccount.AdditionalAccountIdentifier(
+                                        id = "bankAccounts.additionalAccountIdentifiers.id",
+                                        scheme = "bankAccounts.additionalAccountIdentifiers.scheme"
+                                    )
+                                ),
+                                accountIdentification = Submission.Candidate.Details.BankAccount.AccountIdentification(
+                                    id = "bankAccounts.accountIdentification.id",
+                                    scheme = "bankAccounts.accountIdentification.scheme"
+                                ),
+                                address = Submission.Candidate.Details.BankAccount.Address(
+                                    streetAddress = "streetAddress",
+                                    postalCode = "postalCode",
+                                    addressDetails = Submission.Candidate.Details.BankAccount.Address.AddressDetails(
+                                        country = Submission.Candidate.Details.BankAccount.Address.AddressDetails.Country(
+                                            id = "country.id",
+                                            scheme = "country.scheme",
+                                            description = "country.description"
+                                        ),
+                                        region = Submission.Candidate.Details.BankAccount.Address.AddressDetails.Region(
+                                            id = "region.id",
+                                            scheme = "region.scheme",
+                                            description = "region.description"
+                                        ),
+                                        locality = Submission.Candidate.Details.BankAccount.Address.AddressDetails.Locality(
+                                            id = "locality.id",
+                                            scheme = "locality.scheme",
+                                            description = "locality.description"
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
 }
