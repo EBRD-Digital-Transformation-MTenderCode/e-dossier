8ef526db321f1b6a509c746cde0793b1a92f9cb8
yevhenii <yevheniiskoryi@gmail.com>
2020-06-17 13:18:38 +0300

    ES-3534 added application level dto for command 'verifySubmissionPeriodEnd'

---
+++ src/main/kotlin/com/procurement/dossier/application/service/params/VerifySubmissionPeriodEndParams.kt
@@ -0,0 +1,36 @@
+package com.procurement.dossier.application.service.params
+
+import com.procurement.dossier.application.model.parseCpid
+import com.procurement.dossier.application.model.parseDate
+import com.procurement.dossier.application.model.parseOcid
+import com.procurement.dossier.domain.fail.error.DataErrors
+import com.procurement.dossier.domain.model.Cpid
+import com.procurement.dossier.domain.model.Ocid
+import com.procurement.dossier.domain.util.Result
+import com.procurement.dossier.domain.util.asSuccess
+import java.time.LocalDateTime
+
+class VerifySubmissionPeriodEndParams private constructor(
+    val cpid: Cpid,
+    val ocid: Ocid,
+    val date: LocalDateTime
+) {
+    companion object {
+        fun tryCreate(
+            cpid: String,
+            ocid: String,
+            date: String
+        ): Result<VerifySubmissionPeriodEndParams, DataErrors> {
+
+            val parsedCpid = parseCpid(value = cpid)
+                .orForwardFail { fail -> return fail }
+            val parsedOcid = parseOcid(value = cpid)
+                .orForwardFail { fail -> return fail }
+            val parsedDate = parseDate(value = cpid, attributeName = "date")
+                .orForwardFail { fail -> return fail }
+
+            return VerifySubmissionPeriodEndParams(cpid = parsedCpid,ocid = parsedOcid,date = parsedDate)
+                .asSuccess()
+        }
+    }
+}
