83eed8ecd00651c1788d2aae6da8d2fb566a0a75
katherine <kate.shaidurova@gmail.com>
2020-07-10 12:01:49 +0300

    ES-3745 Added GetSubmissionsByQualificationIds request and result

---
+++ src/main/kotlin/com/procurement/dossier/infrastructure/model/dto/request/submission/GetSubmissionsByQualificationIdsRequest.kt
@@ -0,0 +1,14 @@
+package com.procurement.dossier.infrastructure.model.dto.request.submission
+
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class GetSubmissionsByQualificationIdsRequest(
+    @param:JsonProperty("cpid") @field:JsonProperty("cpid") val cpid: String,
+    @param:JsonProperty("ocid") @field:JsonProperty("ocid") val ocid: String,
+    @param:JsonProperty("qualifications") @field:JsonProperty("qualifications") val qualifications: List<Qualification>
+) {
+    data class Qualification(
+        @param:JsonProperty("id") @field:JsonProperty("id") val id: String
+    )
+}
